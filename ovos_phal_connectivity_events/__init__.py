1a64e872dd1b3f577ce18599ad6219cf834461f2
Daniel McKnight <daniel@neongecko.com>
2022-12-09 18:12:55 -0800

    Add unit tests and automation Add documentation to plugin methods Make scheduled checks configurable

--- ovos_phal_connectivity_events/__init__.py
+++ ovos_phal_connectivity_events/__init__.py
@@ -3,6 +3,7 @@ from enum import IntEnum
 
 from mycroft_bus_client import Message
 from ovos_plugin_manager.phal import PHALPlugin
+from ovos_utils.log import LOG
 from ovos_utils.network_utils import is_connected_dns, is_connected_http
 
 
@@ -38,37 +39,52 @@ class ConnectivityEvents(PHALPlugin):
         self.sleep_time = 60
         self.state = ConnectivityState.UNKNOWN
         self.bus.on("ovos.PHAL.internet_check", self.handle_check)
-        self.bus.emit(Message("ovos.PHAL.internet_check"))
+        if not self.config.get('disable_scheduled_checks'):
+            self.bus.emit(Message("ovos.PHAL.internet_check"))
 
-    def update_state(self, state, message):
-        if state == ConnectivityState.FULL:
-            # has internet
-            if self.state <= ConnectivityState.NONE:
+    def update_state(self, new_state: ConnectivityState, message: Message):
+        """
+        Handle a state change and emit the appropriate bus message(s).
+        emits `mycroft.network.connected` if network is newly connected
+        emits `mycroft.internet.connected` if internet is newly connected
+        emits `mycroft.network.disconnected` if network is newly disconnected
+        emits `mycroft.internet.disconnected` if network is newly disconnected
+        emits `enclosure.notify.no_internet` if new state has
+            some networking but no internet
+        :param new_state: Current connection state
+        :param message: Message associated with request to check internet state
+        """
+        LOG.info(f"Network state changed to: {new_state.value}")
+        if new_state == ConnectivityState.FULL:  # Gained internet
+            if self.state <= ConnectivityState.NONE:  # Gained network
                 self.bus.emit(message.reply("mycroft.network.connected"))
             self.bus.emit(message.reply("mycroft.internet.connected"))
-        elif state > ConnectivityState.NONE:
-            # connected to network, but no internet)
-            if self.state <= ConnectivityState.NONE:
+        elif new_state > ConnectivityState.NONE:  # Gained non-internet network
+            if self.state <= ConnectivityState.NONE:  # Gained network
                 self.bus.emit(message.reply("mycroft.network.connected"))
-            if self.state >= ConnectivityState.FULL:
+            if self.state >= ConnectivityState.FULL:  # Lost internet
                 self.bus.emit(message.reply("mycroft.internet.disconnected"))
-        else:
-            # no internet, not connected
-            if self.state >= ConnectivityState.FULL:
+                self.bus.emit(message.reply("enclosure.notify.no_internet"))
+        else:  # Lost some amount of networking
+            if self.state >= ConnectivityState.FULL:  # Lost internet
                 self.bus.emit(message.reply("mycroft.internet.disconnected"))
-            if self.state >= ConnectivityState.NONE:
+            if self.state >= ConnectivityState.NONE:  # Lost network
                 self.bus.emit(message.reply("mycroft.network.disconnected"))
             self.bus.emit(message.reply("enclosure.notify.no_internet"))
 
-        self.state = state
+        self.state = new_state
         if self.state == ConnectivityState.FULL:
-            self.bus.emit(message.reply("mycroft.internet.state", {"state": "connected"}))
+            self.bus.emit(message.reply("mycroft.internet.state",
+                                        {"state": "connected"}))
         else:
-            self.bus.emit(message.reply("mycroft.internet.state", {"state": "disconnected"}))
+            self.bus.emit(message.reply("mycroft.internet.state",
+                                        {"state": "disconnected"}))
         if self.state > ConnectivityState.NONE:
-            self.bus.emit(message.reply("mycroft.network.state", {"state": "connected"}))
+            self.bus.emit(message.reply("mycroft.network.state",
+                                        {"state": "connected"}))
         else:
-            self.bus.emit(message.reply("mycroft.network.state", {"state": "disconnected"}))
+            self.bus.emit(message.reply("mycroft.network.state",
+                                        {"state": "disconnected"}))
 
     def handle_check(self, message):
         if not is_connected_dns():
