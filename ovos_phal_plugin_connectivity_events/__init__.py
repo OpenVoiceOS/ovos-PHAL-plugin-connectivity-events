8ad8f97b75f9c10f2f1889cf2668a55b51b957ea
Daniel McKnight <daniel@neongecko.com>
2023-06-21 08:39:19 -0700

    Refactor `network_requirements` to `runtime_requirements`

--- ovos_phal_plugin_connectivity_events/__init__.py
+++ ovos_phal_plugin_connectivity_events/__init__.py
@@ -6,7 +6,7 @@ from ovos_plugin_manager.phal import PHALPlugin
 from ovos_utils.log import LOG
 from ovos_utils.network_utils import is_connected_dns, is_connected_http
 from ovos_utils import classproperty
-from ovos_utils.network_utils import NetworkRequirements
+from ovos_utils.process_utils import RuntimeRequirements
 
 
 class ConnectivityState(IntEnum):
@@ -45,39 +45,13 @@ class ConnectivityEvents(PHALPlugin):
         self.bus.on("ovos.PHAL.internet_check", self.handle_check)
 
     @classproperty
-    def network_requirements(self):
-        """ developers should override this if they do not require connectivity
-         some examples:
-         IOT plugin that controls devices via LAN could return:
-            scans_on_init = True
-            NetworkRequirements(internet_before_load=False,
-                                 network_before_load=scans_on_init,
-                                 requires_internet=False,
-                                 requires_network=True,
-                                 no_internet_fallback=True,
-                                 no_network_fallback=False)
-         online search plugin with a local cache:
-            has_cache = False
-            NetworkRequirements(internet_before_load=not has_cache,
-                                 network_before_load=not has_cache,
-                                 requires_internet=True,
-                                 requires_network=True,
-                                 no_internet_fallback=True,
-                                 no_network_fallback=True)
-         a fully offline plugin:
-            NetworkRequirements(internet_before_load=False,
-                                 network_before_load=False,
-                                 requires_internet=False,
-                                 requires_network=False,
-                                 no_internet_fallback=True,
-                                 no_network_fallback=True)
-        """
-        return NetworkRequirements(internet_before_load=False,
-                                 network_before_load=False,
-                                 requires_internet=False,
-                                 requires_network=False,
-                                 no_internet_fallback=True,
-                                 no_network_fallback=True)
+    def runtime_requirements(self):
+        return RuntimeRequirements(internet_before_load=False,
+                                   network_before_load=False,
+                                   requires_internet=False,
+                                   requires_network=False,
+                                   no_internet_fallback=True,
+                                   no_network_fallback=True)
 
     def run(self):
         if self.config.get("disable_scheduled_checks"):
